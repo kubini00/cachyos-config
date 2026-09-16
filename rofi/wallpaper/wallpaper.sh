250fca821094509a0a09aeec24622b67e28242e8
kubini00 <71510263+kubini00@users.noreply.github.com>
2026-09-16 22:40:38 +0200

    Add files via upload

---
+++ rofi/wallpaper/wallpaper.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+walldir="${HOME}/Obrazy/wp/"
+cacheDir="${HOME}/.cache/jp"
+
+
+rofi_command="rofi -dmenu \
+-theme ${HOME}/.config/rofi/wallpaper/style.rasi"
+
+mkdir -p "${cacheDir}"
+
+for imagen in "$walldir"/*.{jpg,jpeg,png,webp}; do
+    [ -f "$imagen" ] || continue
+
+    nombre_archivo=$(basename "$imagen")
+
+    if [ ! -f "${cacheDir}/${nombre_archivo}" ]; then
+convert -strip "$imagen" \
+    -resize 640x360 \
+    -background none \
+    -gravity center \
+    -extent 640x360 \
+    "${cacheDir}/${nombre_archivo}"
+    fi
+done
+
+wall_selection=$(
+find "${walldir}" -maxdepth 1 -type f \
+\( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) \
+-exec basename {} \; | sort | while read -r A; do
+    echo -en "$A\x00icon\x1f${cacheDir}/$A\n"
+done | $rofi_command
+)
+
+[[ -n "$wall_selection" ]] || exit 1
+
+sh huj.sh "${walldir}/${wall_selection}"
+mmsg dispatch reload_config
+swaync-client -rs
+
+exit 0
