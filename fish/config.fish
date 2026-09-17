06c9f6ed1f52c8fae467e23f9848eb19f70ae71a
kubini00 <71510263+kubini00@users.noreply.github.com>
2026-09-17 20:10:56 +0200

    Initialize fish configuration with greeting and aliases
    Add custom greeting function and several aliases for commands.

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+function fish_greeting
+    fastfetch
+end
+
+alias huj='sh ~/huj.sh'
+alias clock='tty-clock -c C 1'
+alias fu='flatpak update'
+alias update='yay -S'
+alias ls='eza -l --icons=always'
+alias la='eza -la --icons=always'
+alias ll='eza --icons=always'
