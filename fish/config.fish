383e600505b2ac7ccdf6f1d0ab6a20b76cc6aae5
kubini00 <71510263+kubini00@users.noreply.github.com>
2026-09-17 20:12:04 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ end
 
 alias clock='tty-clock -c C 1'
 alias fu='flatpak update'
-alias update='yay -S'
+alias update='yay -Syu'
 alias ls='eza -l --icons=always'
 alias la='eza -la --icons=always'
 alias ll='eza --icons=always'
